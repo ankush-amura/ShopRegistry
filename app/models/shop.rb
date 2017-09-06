7d858076753f5452e9c23676dea8ee5d641f3617
ankush-amura <ankush@amuratech.com>
2017-09-06 13:14:24 +0530

    changes added rspec into project

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
 class Shop < ApplicationRecord
 
   # shop belongs to property as a table that acts as a refrence table for other models
-  belongs_to :property , polymorphic: true
+  belongs_to :property , polymorphic:true
 
   # each shop is said to have many reviews that are given by the customers
   has_many   :reviews
